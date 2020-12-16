24cc07200e53f7c6fa64410e12756829ba4f32ce
carterquartararo <72881991+carterquartararo@users.noreply.github.com>
2020-12-16 12:54:24 -0800

    Updated home

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("base.html")
+    return render_template("home.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port='5000', host='127.0.0.1')
