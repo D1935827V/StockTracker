7defc15de4c978eaf156cc5279bde5b199f6786e
D1935827V <dvestal2102@gmail.com>
2020-12-18 13:37:06 -0800

    price

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-##from urllib.request import urlopen
-#url = "https://investor.vanguard.com/search/?query=FLR"
-#page = urlopen(url)
-#html_bytes = page.read()
-#html = html_bytes.decode("utf-8")
-#print(html)
-
-import re
-import urllib.request
-response = urllib.request.urlopen('https://investor.vanguard.com/search/?query=FLR')
-html = response.read()
-text = html.decode()
-re.findall('(.*?)',text)
-print(text)
 
 from flask import Flask, render_template
 
--- web_scrape.py
+++ web_scrape.py
@@ -1,14 +1,19 @@
 import requests
 
-url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete"
+stock = "VNQ"
 
-querystring = {"q":"tesla","region":"US"}
+url = "https://alpha-vantage.p.rapidapi.com/query"
+
+querystring = {"function":"GLOBAL_QUOTE","symbol":stock}
 
 headers = {
     'x-rapidapi-key': "90d24f17c2mshe7a5019d169744ep146773jsna039703c4990",
-    'x-rapidapi-host': "apidojo-yahoo-finance-v1.p.rapidapi.com"
+    'x-rapidapi-host': "alpha-vantage.p.rapidapi.com"
 }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
 
 print(response.text)
+
+result = response.json().get("Global Quote")
+print(result['05. price'])
